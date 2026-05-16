18d61b5df53e976e155d6e0d46b04ac3c5ee73bd
Leonardo Pinto <leonardopintosanchez123@gmail.com>
2026-05-16 08:21:00 -0400

    feat(Serilog): add and config serilog

--- Program.cs
+++ Program.cs
@@ -1,8 +1,17 @@
+using Serilog;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 
+#region Logging Configuration
+builder.Host.UseSerilog((context, services, configuration) => configuration
+    .ReadFrom.Configuration(context.Configuration)
+    .ReadFrom.Services(services));
+#endregion
+
 var app = builder.Build();
+
+Log.Information("Starting the application...");
 
 app.MapOpenApi();
 
